d8804211b9f1489ff8067896d16f8bd1432b1fa4
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-07-18 16:54:14 +0300

    Update BasicDateRangeCalendar.jsx

--- src/comp/admin/sahelForm/BasicDateRangeCalendar.jsx
+++ src/comp/admin/sahelForm/BasicDateRangeCalendar.jsx
@@ -4,18 +4,25 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Box } from "@mui/material";
-import dayjs from "dayjs";
 
 function BasicDateRangeCalendar({ onDateChange, startDate, endDate, lang }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box sx ={{flexDirection:{xs:"column",md:"row"}}} display="flex" gap={2} width="100%" justifyContent="space-between">
+      <Box
+        sx={{ flexDirection: { xs: "column", md: "row" } }}
+        display="flex"
+        gap={2}
+        width="100%"
+        justifyContent="space-between"
+      >
         <DatePicker
+          sx={{ width: "100%" }}
           value={startDate}
           onChange={(newValue) => onDateChange("startDate", newValue)}
           label={lang === "ar" ? "تاريخ البداية" : "Start Date"}
         />
         <DatePicker
+          sx={{ width: "100%" }}
           value={endDate}
           onChange={(newValue) => onDateChange("endDate", newValue)}
           label={lang === "ar" ? "تاريخ النهاية" : "End Date"}
